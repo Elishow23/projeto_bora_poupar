0349d6cbefadf18fbc0e4dd1461b9b4e163b2ac2
Elias dos Santos Cosme <ecosme015@gmail.com>
2026-03-02 22:14:18 -0300

    subindo funcionalidade relatorio e finalizando o CRUD

--- projeto/django/me_poupe/urls.py
+++ projeto/django/me_poupe/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from me_poupe.views import index, despesa, relatorio, cadastro_despesa, listar_despesas, excluir_despesa
+from me_poupe.views import index, despesa, relatorio, cadastro_despesa, listar_despesas, excluir_despesa, editar_despesa, pesquisar
 
 
 urlpatterns = [
 
   path('', index, name='index'),
   path('despesa/', despesa, name='despesa'),
-  path('relatorio/', relatorio, name='relatorio'),
+  path('despesa/relatorio/', relatorio, name='relatorio'),
   path('despesa/cadastro/', cadastro_despesa, name='cadastro_despesa'),
   path('despesa/listar/', listar_despesas, name='listar_despesas'),
   path('despesa/excluir/<int:id>/', excluir_despesa, name='excluir_despesa'),
+  path('despesa/editar/<int:id>/', editar_despesa, name='editar_despesa'),
+  path('despesa/pesquisar/', pesquisar, name='pesquisar'),
 ]
--- projeto/django/me_poupe/views.py
+++ projeto/django/me_poupe/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Despesa
-from django.shortcuts import redirect
 from django.db.models import Sum
+import json
+
 
 
 # Create your views here.
@@ -57,3 +58,55 @@ def excluir_despesa(request, id):
     despesa = get_object_or_404(Despesa, id=id)
     despesa.delete()
     return redirect('cadastro_despesa')
+
+def editar_despesa(request, id):
+    despesa = get_object_or_404(Despesa, id=id)
+
+    if request.method == 'POST':
+        despesa.descricao = request.POST.get('descricao')
+        despesa.valor = request.POST.get('valor')
+        despesa.data = request.POST.get('data')
+        despesa.categoria = request.POST.get('categoria')
+        despesa.save()
+
+        return redirect('listar_despesas')
+
+    despesas = Despesa.objects.all()
+    total = despesas.aggregate(Sum('valor'))['valor__sum'] or 0
+
+    return render(request, 'despesa.html', {
+        'despesas': despesas.order_by('-data'),
+        'total': total,
+        'despesa': despesa
+    })
+
+def pesquisar(request):
+    query = request.GET.get('q')
+
+    despesas = Despesa.objects.all()
+
+    if query:
+        despesas = despesas.filter(descricao__icontains=query)
+
+    total = despesas.aggregate(Sum('valor'))['valor__sum'] or 0
+
+    return render(request, 'despesa.html', {
+        'despesas': despesas.order_by('-data'),
+        'total': total
+    })
+
+def relatorio(request):
+    dados = (
+        Despesa.objects
+        .values('categoria')
+        .annotate(total=Sum('valor'))
+        .order_by('categoria')
+    )
+
+    categorias = [item['categoria'] for item in dados]
+    totais = [float(item['total']) for item in dados]
+
+    return render(request, 'relatorio.html', {
+        'categorias': json.dumps(categorias),
+        'totais': json.dumps(totais)
+    })
